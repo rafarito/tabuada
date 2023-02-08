21002b540e691e073857bd92028c4048b4027bc8
Rafael Matos <124383453+rafarito@users.noreply.github.com>
2023-02-08 14:25:09 -0300

    Update tabuada.java

--- tabuada.java
+++ tabuada.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
 public class App {
+    public static int leValor(String str) 
+    {
+        if(str.matches("[a-zA-Z]+")){
+            System.out.println("\nvocê não digitou um valor valido");
+            return 0;
+        }else{
+            int numero = Integer.parseInt(str);
+            return numero;
+        }
+    }
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
-
-
         System.out.print("digite un numero:");
-        int num = teclado.nextInt();
-        if(num.matches("[a-zA-Z]+")){ //trabalhe nisso
-            System.out.print("true");
-        }else{
-            System.out.print("false");
-        }
+        int num = leValor(teclado.nextLine());
         System.out.print("\ndigite uma operação:\n   digite 1 para soma\n   digite 2 para subtração\n   digite 3 para multiplicação\n   digite 4 para divisão\n");
-        int op = teclado.nextInt();
+        int op = leValor(teclado.nextLine());
         for(int i = 1;i <= 10;i++){
             if(op == 1){
                 System.out.printf("%d + %d = %d\n",num, i, num+i);
@@ -36,3 +39,4 @@ public class App {
         }
     }
 }
+
