afed6cc3ec614a67da92bc343e8c4300c31f8338
Rafael Matos <124383453+rafarito@users.noreply.github.com>
2023-02-08 16:54:55 -0300

    otimização e correções
    a escolha de operações foi otimizada e agora o sistema controla os valores de input

--- tabuada.java
+++ tabuada.java
@@ -4,8 +4,7 @@ public class App {
     public static int leValor(String str) 
     {
         if(str.matches("[a-zA-Z]+")){
-            System.out.println("\nvocê não digitou um valor valido");
-            return 0;
+            return 2147483647;
         }else{
             int numero = Integer.parseInt(str);
             return numero;
@@ -13,30 +12,37 @@ public class App {
     }
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
+        int num;
+        do{
         System.out.print("digite un numero:");
-        int num = leValor(teclado.nextLine());
+        num = leValor(teclado.nextLine());
+        }while(num == 2147483647);
         System.out.print("\ndigite uma operação:\n   digite 1 para soma\n   digite 2 para subtração\n   digite 3 para multiplicação\n   digite 4 para divisão\n");
         int op = leValor(teclado.nextLine());
-        for(int i = 1;i <= 10;i++){
-            if(op == 1){
-                System.out.printf("%d + %d = %d\n",num, i, num+i);
-            }else{
-                if(op == 2){
-                    System.out.printf("%d - %d = %d\n",num, i, num-i);
-                }else{
-                    if(op == 3){
-                        System.out.printf("%d * %d = %d\n",num, i, num*i);
-                    }else{
-                        if(op == 4){
-                            System.out.printf("%d / %d = %d\n",num, i, num/i);
-                        }else{
-                            System.out.println("você não digitou um valor valido");
-                            break;
-                        }
-                    }
+        switch (op) {
+            case 1:
+                for (int i = 1; i <= 10; i++) {
+                    System.out.printf("%d + %d = %d\n",num, i, num+i);
                 }
-            }
+                break;
+            case 2:
+                for (int i = 1; i <= 10; i++) {
+                    System.out.printf("%d - %d = %d\n",num, i, num-i);
+                }
+                break;
+            case 3:
+                for (int i = 1; i <= 10; i++) {
+                    System.out.printf("%d * %d = %d\n",num, i, num*i);
+                }
+                break;
+            case 4:
+                for (int i = 1; i <= 10; i++) {
+                    System.out.printf("%d / %d = %d\n",num, i, num/i);
+                }
+                break;
+            default:
+                System.out.println("você não digitou um valor válido");
+                break;
         }
     }
 }
-
